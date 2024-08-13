674f91e0e3e910c81cba31e351564f8bbdaa115c
Graham Gill <graham.gill@gmail.com>
2024-08-12 23:47:12 -0400

    start setup for unittest

---
+++ tests/test_rounder.py
@@ -0,0 +1,11 @@
+import unittest
+from roundingutils import *
+
+class TestRounderMethods(unittest.TestCase):
+    
+    def test_Ints(self):
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
